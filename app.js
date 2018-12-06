15e10cf3139b63fc092929ec47bff1ff0cfa0d3e
s0njablade <sonjita.chacon@gmail.com>
2018-12-06 12:33:37 -0700

    body parser

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const queries = require('./queries')
-const bodyParser = require('body-parer')
-
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
 
 app.use(bodyParser.json())
+app.use(cors())
 
+app.listen(port, () => {
+    console.log(`running on port: ${port}`)
+})
